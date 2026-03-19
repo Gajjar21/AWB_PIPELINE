6ddef9ac71c7192b6a6a5cabbd83a3a8d7d41959
Gajjar21 <harshgjjr@gmail.com>
2026-03-18 19:36:05 -0700

    Add setup.py for pip installation configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='awb_pipeline',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add your project dependencies here
+    ],
+    entry_points={
+        'console_scripts': [],
+    },
+)
